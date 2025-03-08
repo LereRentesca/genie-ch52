a4e5aa5c4ac8b8cc0701cd7857c8f94c80d15899
LereRentesca <esteban.renteria@uabc.edu.mx>
2025-03-08 08:06:37 -0800

    Class 03

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+class Status(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    subtitle = models.CharField(max_length=256)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete = models.CASCADE
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.ForeignKey(
+        Status,
+        null=True,
+        default=None,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
